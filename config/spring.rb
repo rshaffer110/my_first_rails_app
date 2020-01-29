2158fbafd9c66adccab1bedcf7d768bf358f2bee
rshaffer110 <rshaffer110@gmail.com>
2020-01-29 18:37:38 -0500

    updates for heroku deployment

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,4 @@
-%w[
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-].each { |path| Spring.watch(path) }
+Rails.application.routes.draw do
+  root 'cars#index'
+  resources :cars
+end
